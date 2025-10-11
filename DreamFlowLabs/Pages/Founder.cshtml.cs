3fd485d2d42d1e90b2a1d612cdcee9938a14fb28
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-10-11 08:07:46 -0400

    Add founder page

---
+++ DreamFlowLabs/Pages/Founder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DreamFlowLabs.Pages
+{
+    public class FounderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
