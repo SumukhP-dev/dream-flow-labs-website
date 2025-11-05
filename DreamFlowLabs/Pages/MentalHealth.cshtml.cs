0888e18c08762f8fd52e02e736e845c210ef3aa8
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-11-05 15:56:46 -0500

    Add Dreamflow Notes page

---
+++ DreamFlowLabs/Pages/MentalHealth.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DreamflowLabs.Pages
+{
+    public class MentalHealthModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
